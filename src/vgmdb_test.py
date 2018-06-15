e63717c503344dd44bac0fbe09d312d594d88d18
Vinsen Muliadi <admin@max-metal.us>
2018-06-15 16:12:45 +0700

    Replacing Unit Test Assertion with Regex

--- src/vgmdb_test.py
+++ src/vgmdb_test.py
@@ -2,11 +2,12 @@
 
 import json
 import pytest
+import re
 from vgmdb import get_vgmdb
 # print(get_vgmdb('https://vgmdb.net/album/11939'))
 
 def test_json_output():
 	json_response = json.loads(get_vgmdb('https://vgmdb.net/album/11939'))
-	assert 2009 == json_response['release_year']
-	assert 0 < len(json_response['track_info'])
-	assert 'DERD-10001' == json_response['catalog_number']
+	assert bool(re.search(r'\d{2}:\d{2}', json_response['total_length'])) == True
+	assert json_response['release_year'].__class__ == int \
+		and bool(re.search(r'\d{4}', str(json_response['release_year']))) == True
