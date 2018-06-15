c5809f1fe5355857cfa49e0a995a0383aba7edc7
Vinsen Muliadi <admin@max-metal.us>
2018-06-15 13:56:02 +0700

    Adding Unit Test and Python Packages

--- src/vgmdb.py
+++ src/vgmdb.py
@@ -2,15 +2,16 @@
 
 import json
 import requests
+from bs4 import BeautifulSoup
 
 def get_vgmdb(vgmdb_url):
     album_information = {}
-    vgmdb_request = requests.get(vgmdb_source)
+    vgmdb_request = requests.get(vgmdb_url)
     scraping_request = BeautifulSoup(vgmdb_request.text, 'html.parser')
     general_info = scraping_request.findAll('table', {'id':'album_infobit_large'})[0]
     album_information['catalog_number'] = general_info.findAll('tr')[0].findAll('td')[1].get_text().split()[0]
     try:
-        album_information['release_year'] = general_info.findAll('tr')[1].findAll('td')[1].get_text().split(',')[1].split()[0]
+        album_information['release_year'] = int(general_info.findAll('tr')[1].findAll('td')[1].get_text().split(',')[1].split()[0])
         album_information['publish_format'] = general_info.findAll('tr')[2].findAll('td')[1].get_text()
         album_information['release_price'] = general_info.findAll('tr')[3].findAll('td')[1].get_text()
         album_information['media_format'] = general_info.findAll('tr')[4].findAll('td')[1].get_text()
@@ -20,7 +21,7 @@ def get_vgmdb(vgmdb_url):
         album_information['arranged_by'] = general_info.findAll('tr')[8].findAll('td')[1].get_text()
         album_information['performed_by'] = general_info.findAll('tr')[9].findAll('td')[1].get_text()
     except IndexError:
-        album_information['release_year'] = general_info.findAll('tr')[4].findAll('td')[1].get_text().split(',')[1].split()[0]
+        album_information['release_year'] = int(general_info.findAll('tr')[4].findAll('td')[1].get_text().split(',')[1].split()[0])
         album_information['publish_format'] = general_info.findAll('tr')[5].findAll('td')[1].get_text()
         album_information['release_price'] = general_info.findAll('tr')[6].findAll('td')[1].get_text()
         album_information['media_format'] = general_info.findAll('tr')[7].findAll('td')[1].get_text()
@@ -42,5 +43,4 @@ def get_vgmdb(vgmdb_url):
     album_information['total_length'] = scraping_request.findAll('div', {'id':'tracklist'})[0]\
         .findAll('span', {'class':'tl'})[0].findAll('span')[-1].get_text()
 
-    return album_information
-
+    return json.dumps(album_information, indent=4, sort_keys=False)
